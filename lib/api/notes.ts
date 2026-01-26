adeb301f0266ef01df06bfbb71b3242644b6c208
Ivan227-hub <i.kushkov-de21@lll.kpi.ua>
2026-01-26 14:37:24 +0200

    fixed2

--- lib/api/notes.ts
+++ lib/api/notes.ts
@@ -4,12 +4,12 @@ import { Note } from "@/types/note";
 const api = axios.create({
   baseURL: "https://notehub-public.goit.study/api",
   headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
-  },
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`
+  }
 });
 
 export const fetchNotes = async (tag?: string): Promise<Note[]> => {
-  const params = tag ? { tag } : {};
+  const params = tag && tag !== "all" ? { tag } : {};
   const { data } = await api.get("/notes", { params });
   return data.notes;
 };

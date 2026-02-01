707068744c6f536d6eea650033d01fb061004b0f
Ivan227-hub <i.kushkov-de21@lll.kpi.ua>
2026-02-01 11:13:38 +0200

    hw07

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,10 +1,38 @@
-import Modal from "@/components/Modal/Modal";
-import NotePreview from "./NotePreview.client";
+"use client";
 
-export default function Page({ params }: { params: { id: string } }) {
+import { useRouter } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import { fetchNoteById } from "@/lib/api"; 
+import { Note } from "@/types/note";
+import Modal from "@/components/Modal/Modal";
+
+interface NotePageProps {
+  params: { id: string };
+}
+
+export default function NotePage({ params }: NotePageProps) {
+  const { id } = params;
+  const router = useRouter();
+
+  
+  const { data, isLoading, isError } = useQuery<Note>({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  if (isLoading) return <p>Loading note...</p>;
+  if (isError || !data) return <p>Failed to load note.</p>;
+
   return (
     <Modal>
-      <NotePreview id={params.id} />
+      <button
+        onClick={() => router.back()}
+        style={{ marginBottom: "1rem" }}
+      >
+        Close
+      </button>
+      <h2>{data.title}</h2>
+      <p>{data.content}</p>
     </Modal>
   );
 }

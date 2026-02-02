86eba80467425c93dbe632fb0589086812daea75
Ivan227-hub <i.kushkov-de21@lll.kpi.ua>
2026-02-02 12:20:22 +0200

    HW07

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -4,11 +4,12 @@ import { useRouter } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
 import Modal from "@/components/Modal/Modal";
+import { Note } from "@/types/note";
 
 export default function NotePreview({ id }: { id: string }) {
   const router = useRouter();
 
-  const { data, isLoading, isError } = useQuery({
+  const { data, isLoading, isError } = useQuery<Note>({
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
     refetchOnMount: false,
@@ -22,7 +23,7 @@ export default function NotePreview({ id }: { id: string }) {
     <Modal onClose={() => router.back()}>
       <h2>{data.title}</h2>
       <p>{data.content}</p>
-      <p>{data.tags?.join(", ")}</p>
+      <p>{data.tag}</p> {/* <- исправлено */}
       <small>{new Date(data.createdAt).toLocaleDateString()}</small>
     </Modal>
   );
